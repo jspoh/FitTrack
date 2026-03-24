93b52d91f7a393f8e331a108a496174dc4aa0d54
jspoh <hello@jspoh.dev>
2026-03-24 18:19:16 +0000

    server update

--- server/init.sh
+++ server/init.sh
@@ -1,5 +1,3 @@
 python3 -m venv venv
 venv/bin/pip3 install -r requirements.txt
-venv/bin/alembic init migrations
-venv/bin/alembic revision --autogenerate -m "initial migration"
 venv/bin/alembic upgrade head
